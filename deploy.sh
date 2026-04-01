3e2289f46f52e5a3aa52eee436132acab083f94c
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2026-04-01 11:59:08 +0800

    更新 package.json 和 package-lock.json，新增 hexo-cli 依赖并修正 hexo 版本

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 hexo g && hexo d
-npm install
-npm install hexo --save
-npm install hexo-cli --save
-npm run build && npm run deploy
+# npm install
+# npm install hexo --save
+# npm install hexo-cli --save
+# npm run build && npm run deploy
